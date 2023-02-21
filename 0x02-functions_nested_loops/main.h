8cb00a353bba9c218b3466a87c17c40eb57bcd14
RogersKaran <karanjarodgers@gmail.com>
2023-02-21 12:16:34 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x18(void);
+
+void _islower(int c);
+
+void _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bouer(void);
+
+void times_tablet(void);
+
+int add(int, int);
+
+void Print_to_98(int ni);
+
+void print_times_tabled(int n);
