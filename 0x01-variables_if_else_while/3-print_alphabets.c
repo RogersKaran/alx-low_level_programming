2b229eb99a08e78e0abf3e8ef696321fed68801d
RogersKaran <karanjarodgers@gmail.com>
2023-02-27 01:06:08 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,17 @@
  * main - Display the alphabet letters in uppercase
  *
  * Return: 0 (Run successful).
- *
  */
 
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 
 	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
-	
 	putchar('\n');
 	return (0);
 }
