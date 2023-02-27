b51a0b13b40090e8d9d4be2e265e489755f980c0
RogersKaran <karanjarodgers@gmail.com>
2023-02-27 00:52:28 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	char  c;
 	
-	for (c = '0'; c < '10'; i++)
+	for (c = '0'; c < '10'; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c);
 	}
-	_putchar('\n');   // print a new line character
+	_putchar('\n');
 }
