4f590e05840641ab20ffe21bda821992aaab1a0f
RogersKaran <karanjarodgers@gmail.com>
2023-02-27 20:15:58 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_alphabet_x10 -Make alphabet x10
@@ -10,9 +11,9 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 	int i, j;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = '0'; j < '26'; j++)
+		for (j = 0; j <= 26; j++)
 		{
 			_putchar(letter);
 			letter++;
