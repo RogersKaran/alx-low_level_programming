8a51e31d257c78ac775931ffb96b3ecc66abce95
RogersKaran <karanjarodgers@gmail.com>
2023-02-28 00:42:20 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,13 @@ void puts2(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		_putchar(str[i]);
-	}
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
 	_putchar('\n');
 }
