a3c792b41effce7f1659a94701b3f8e59fc42caf
RogersKaran <karanjarodgers@gmail.com>
2023-03-06 00:17:31 -0800

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Takes a pointer to pointer to a character as arguments
+ * @s: char pointing to a pointer
+ * @to: char acting as argument
+ */
+
+#include <stdio.h>
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
