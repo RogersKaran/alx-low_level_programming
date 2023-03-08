fb55d65d06fc919f0dcc0b5af249571d96772db0
RogersKaran <karanjarodgers@gmail.com>
2023-03-08 07:53:16 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,19 +8,10 @@
  * Return: The length of a string.
  */
 
-int main(void)
-{
-        char str[] = "Hello, world!";
-
-        printf("The length of string '%s' is %d\n", str, _strlen_recursion(str));
-        return (0);
-}
-
 int _strlen_recursion(char *s)
 {
-	if
-		(*s == '\0')
-			return (0);
+	if (*s == '\0')
+		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
