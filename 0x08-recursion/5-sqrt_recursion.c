d9f2a517a0d589513184a9e415ea4e3b8586bf19
RogersKaran <karanjarodgers@gmail.com>
2023-03-09 11:05:17 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _sqrt_recursion - Recursive fxn calcualating the natural sqrt using bin
- * @n:Input that returns an int rep.ing the naural sqrt of 'n'.
- * @low:
- * @high:
+ * sqrt_find - first checks for the square root of given number
+ * @g: the root to check through
+ * @c: number to find sqrt of
  *
- * Return:  -1 (error)
+ * Return: The sqrt of C or -1 if number does not have natural sqrt
+ */
+
+int sqrt_find(int g, int c)
+{
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_find(g + 1, c));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find sqrt of
+ *
+ * Return: natural square root or -1(if not natural square root)
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 1)
-		return (n);
-	else if (n  == 0)
-		return (-1);
-	else
-	{
-		int low = 0;
-		int high = n;
-		int mid = (low + high) / 2;
-		int sqrt;
-
-		if (mid * mid == n)
-			sqrt = mid;
-		else if (mid * mid < n)
-			sqrt = _sqrt_recursion(mid + 1);
-		else
-			sqrt = _sqrt_recursion(mid - 1);
-		return (sqrt);
-	}
-	return(0);
+	if (n == 0)
+		return (0);
+	return (sqrt_find(1, n));
 }
