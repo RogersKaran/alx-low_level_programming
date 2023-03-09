6ab2b8070670a41a2d439f88eeaa356df6d3123f
RogersKaran <karanjarodgers@gmail.com>
2023-03-09 11:46:13 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,12 +7,34 @@
  * Return: 1 (success)
  */
 
+#include <math.h>
+
 int is_prime_number(int n)
 {
-	if (n >= 1)
+	if (n <= 1)
+		return (0);
+	else if (n == 2)
 		return (1);
-	else if (n <= 0)
+	else if (n % 2 == 0)
 		return (0);
 	else
-		return (n * is_prime_number, (i + 1));
+		return (is_prime_number_helper(n, 3));
+}
+
+/**
+ * is_prime_number_helper - Helps
+ * @n: The number of times
+ * @i:
+ *
+ * Return: 1 (success) or 0 (fail)
+ */
+
+int is_prime_number_helper(int n, int i)
+{
+	if (i > sqrt(n))
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else
+		return (is_prime_number_helper(n, i + 2));
 }
