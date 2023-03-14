7151e7ae3474de7de656bae3a380a194770efdb1
RogersKaran <karanjarodgers@gmail.com>
2023-03-14 03:47:16 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,7 +12,7 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 		return;
 	}
 	else
@@ -21,8 +21,8 @@ void print_line(int n)
 
 		for (i = 0; i < n; i++)
 		{
-			_putchar('_');
+			putchar('_');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
