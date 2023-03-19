604ae08e98b274d6935ec20690ee6790134fbda9
RogersKaran <karanjarodgers@gmail.com>
2023-03-19 01:07:33 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,40 +1,28 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * is_prime_number - Fxn that checks whether an input integer is a prime.
  * @n: Input parameters.
  *
- * Return: 1 (success)
+ * Return: 1 (prime number) & 0 (not prime number)
  */
-
-#include <math.h>
 
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 		return (0);
-	else if (n == 2)
-		return (1);
-	else if (n % 2 == 0)
-		return (0);
-	else
-		return (is_prime_number_helper(n, 3));
-}
 
-/**
- * is_prime_number_helper - Helps
- * @n: The number of times
- * @i:
- *
- * Return: 1 (success) or 0 (fail)
- */
+	i = 2;
 
-int is_prime_number_helper(int n, int i)
-{
-	if (i > sqrt(n))
-		return (1);
-	else if (n % i == 0)
-		return (0);
-	else
-		return (is_prime_number_helper(n, i + 2));
+	while (i * i <= n)
+	{
+		if (n % i == 0)
+			return (0);
+		i++;
+	}
+
+	return (1);
 }
