8ad6d537f55df022f2dcba18351c181e049b814b
RogersKaran <karanjarodgers@gmail.com>
2023-03-19 01:14:45 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,13 +15,18 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 
-	i = 2;
+	if (n <= 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+
+	i = 5;
 
 	while (i * i <= n)
 	{
-		if (n % i == 0)
+		if (n % i == 0 || n % (i + 2) == 0)
 			return (0);
-		i++;
+		i += 6;
 	}
 
 	return (1);
