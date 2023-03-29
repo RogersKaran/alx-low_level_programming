bd98b33b685b112232ffaca1109f267a1a916608
RogersKaran <karanjarodgers@gmail.com>
2023-03-29 01:36:00 -0700

    updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,11 @@
 
 int main(void)
 {
-	printf("char: %ld byte(s)\n", sizeof(char));
-	printf("short: %ld byte(s)\n", sizeof(short));
-	printf("int: %ld byte(s)\n", sizeof(int));
-	printf("long: %ld byte(s)\n", sizeof(long));
-	printf("long long: %ld byte(s)\n", sizeof(long long));
-	printf("float: %ld byte(s)\n", sizeof(float));
-	printf("double: %ld byte(s)\n", sizeof(double));
-	printf("long double: %ld byte(s)\n", sizeof(long double));
-
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(long long));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	
 	return (0);
 }
